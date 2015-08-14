3f1ab72998411d918c0cd0d8eb7e56094e297020
V3506 (Conference ID) <v3506@cam.ac.uk>
2015-08-14 14:05:29 +0100

    attempt at colour response

--- drone-frontend.js
+++ drone-frontend.js
@@ -1,12 +1,14 @@
+var tracker;
+
 function init() {
-    var tracker = initTracker("#example");
-    tracking.track("#example .drone", tracker);
+  tracker = initTracker("#droneView");
+    droneConnection.streamImage(tracker, "#droneView .drone");
 }
 
 function initTracker(element) {
     // Initialise a color tracker
     var tracker = new tracking.ColorTracker();
-
+    tracker.setMinDimension(20);
 
     TrackerUtils.addTrackingColor("#494A45", "red", tracker);
     TrackerUtils.addTrackingColor("#A04242", "red", tracker);
@@ -27,19 +29,20 @@ function initTracker(element) {
     // Whenever there is a new color detected, mark them
     tracker.on('track', function(event) {
         markColors(event.data, element);
-        console.log(event.data);
-
+       // console.log(event.data);
+        decideDroneMovement(event.data);
     });
 
     return tracker;
 }
 
-function markColors(colors, element) {
+
+    function markColors(colors, element) {
     var canvas = $(element + ' .canvas').get(0);
     var context = canvas.getContext('2d');
 
     context.clearRect(0, 0, context.width, context.height);
-
+    $(element +" .output").empty();
     for (var i = 0; i < colors.length; i++) {
         drawRectangle(colors[i], context);
         writeRectangle(colors[i], element + " .output");
@@ -49,6 +52,10 @@ function markColors(colors, element) {
 function drawRectangle(rect, context) {
     context.strokeStyle = rect.color;
     context.strokeRect(rect.x, rect.y, rect.width, rect.height);
+    context.font = '11px Helvetica';
+    context.fillStyle = "#fff";
+    context.fillText('x: ' + rect.x + 'px', rect.x + rect.width + 5, rect.y + 11);
+    context.fillText('y: ' + rect.y + 'px', rect.x + rect.width + 5, rect.y + 22);
 }
 
 function writeRectangle(rect, element) {
@@ -57,6 +64,31 @@ function writeRectangle(rect, element) {
         .append("<p>")
         .append(rect.color + ": " + rect.width + "X" + rect.height)
         .append("@ " + rect.x + ":" + rect.y)
+}
+
+function decideDroneMovement(colors) {
+    var move = {
+        left: false,
+        right: false
+    };
+
+    colors.forEach(function(rectangle) {
+        if (rectangle.color === "green") {
+            if (rectangle.width > 250) {
+                move.left = true;
+            }
+        }
+
+        else if (rectangle.color === "red") {
+            if (rectangle.width > 250) {
+                move.right = true;
+            }
+        }
+
+    });
+
+    console.log("Move", move);
+    droneConnection.send(move);
 }
 
 window.addEventListener("load", init);
