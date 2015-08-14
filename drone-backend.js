ed398608c0c5c68177acaaf4053f28a51d420494
V3506 (Conference ID) <v3506@cam.ac.uk>
2015-08-14 11:51:57 +0100

    drone try 1

--- drone-backend.js
+++ drone-backend.js
@@ -1,5 +1,4 @@
 var Cylon = require('cylon');
-var bot;
 
 // Initialise the robot
 Cylon.robot()
@@ -11,11 +10,58 @@ Cylon.robot()
         driver: "ardrone",
         connection: "ardrone"
     })
+    .device("nav", {
+        driver: "ardrone-nav",
+        connection: "ardrone"
+    })
     .on("ready", fly);
-    
-// Fly the bot
-function fly(robot) {
 
+var bot;
+function fly(robot) {
+    bot = robot;
+
+    bot.drone.config('general:navdata_demo', 'TRUE');
+
+    bot.nav.on("navdata", function(data) {
+        // console.log(data);
+    });
+
+    bot.nav.on("altitudeChange", function(data) {
+        console.log("Altitude:", data);
+
+        if (altitude > 2) {
+            bot.drone.land();
+        }
+    });
+
+    bot.nav.on("batteryChange", function(data) {
+        console.log("Battery level:", data);
+    });
+
+    bot.drone.disableEmergency();
+
+    bot.drone.ftrim();
+
+
+    bot.drone.takeoff();
+
+    bot.drone.left (0.2);
+    after (1*1000, function(){
+        bot.drone.left(0);
+    });
+
+    bot.drone.right (0.2);
+    after (1*1000, function(){
+        bot.drone.right(0);
+    });
+
+    after(8*1000, function() {
+        bot.drone.land();
+    });
+    after(10*1000, function() {
+        bot.drone.stop();
+    });
 }
 
 Cylon.start();
+
